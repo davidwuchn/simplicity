52213e721759a742fa2ffa618fe8f466b76f7830
Wu David <davidwuchn@gmail.com>
2022-01-30 14:00:12 +0800

    use babashka as scripts engine

---
+++ config/build.clj
@@ -0,0 +1,77 @@
+(ns build
+  (:require [babashka.fs :as fs]
+            [babashka.process :as process]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]
+            [nextjournal.cas :as cas]
+            [rewrite-clj.zip :as z]))
+
+(def lib 'io.github.nextjournal/clerk)
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def version (-> (slurp "resources/META-INF/nextjournal/clerk/meta.edn") edn/read-string :version))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn jar [_]
+  (b/delete {:path "target"})
+  (println "Producing jar:" jar-file)
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :scm {:url "http://github.com/nextjournal/clerk"
+                      :connection "scm:git:git://github.com/nextjournal/clerk.git"
+                      :developerConnection "scm:git:ssh://git@github.com/nextjournal/clerk.git"}
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn deploy [opts]
+  (println "Deploying version" jar-file "to Clojars.")
+  (format "target/%s-%s.jar" (name lib) version)
+  (dd/deploy (merge {:installer :remote
+                     :artifact jar-file
+                     :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+                    opts)))
+
+(defn asserting [val msg] (assert (seq val) msg) val)
+
+(def clerk-config-path "src/nextjournal/clerk/config.clj")
+
+(defn replace-resource [resource url]
+  (-> (io/file clerk-config-path)
+      z/of-file
+      (z/find-token z/next #(and (= 'def (z/sexpr %))
+                                 (= 'default-resource-manifest (-> % z/right z/sexpr))))
+      (z/find-value z/next resource)
+      z/right
+      (z/edit (fn [old-url]
+                (println "Replacing resource: " resource "\n"
+                         (if (= old-url url)
+                           (str "url " old-url " is up-to-date.")
+                           (str "updated from " old-url " to " url ".")))
+                url))
+      z/root-string))
+
+(defn upload! [opts file] (:url (cas/upload! opts file)))
+
+(defn update-resource! [opts resource _ file]
+  (spit clerk-config-path
+        (replace-resource resource
+                          (upload! opts file))))
+
+(defn get-gsutil [] (str/trim (:out (process/sh ["which" "gsutil"]))))
+
+(def resource->path
+  '{viewer.js "/js/viewer.js"})
+
+(defn upload-to-cas+rewrite-sha [{:keys [resource]}]
+  (if-let [target (resource->path resource)]
+    (update-resource! {:exec-path (str/trim (asserting (get-gsutil) "Can't find gsutil executable."))
+                       :target-path "gs://nextjournal-cas-eu/data/"} target :uploading (str "build/" resource))
+    (throw (ex-info (str "unsupported resource " resource) {:supported-resources (keys resource->path)}))))
---
+++ config/user.clj
@@ -0,0 +1,40 @@
+(ns user
+  (:require [nextjournal.clerk :as clerk]))
+
+(comment
+  (clerk/serve! {:browse? true}))
+
+(comment
+  ;; start without file watcher
+  (clerk/serve! {})
+
+  ;; start with file watcher
+  (clerk/serve! {:watch-paths ["notebooks" "src"]})
+
+  ;; start with file watcher and show filter function to enable notebook pinning
+  (clerk/serve! {:watch-paths ["notebooks" "src"] :show-filter-fn #(clojure.string/starts-with? % "notebooks")})
+
+  (clerk/show! "notebooks/onwards.md")
+  (clerk/show! "notebooks/rule_30.clj")
+  (clerk/show! "notebooks/how_clerk_works.clj")
+  (clerk/show! "notebooks/pagination.clj")
+  (clerk/show! "notebooks/paren_soup.clj")
+  (clerk/show! "notebooks/recursive.clj")
+  (clerk/show! "notebooks/tap.clj")
+
+  (clerk/show! "notebooks/markdown.md")
+
+  (clerk/show! "notebooks/viewer_api.clj")
+
+
+  (clerk/show! "notebooks/viewers/vega.clj")
+  (clerk/show! "notebooks/viewers/plotly.clj")
+  (clerk/show! "notebooks/viewers/table.clj")
+  (clerk/show! "notebooks/viewers/tex.clj")
+  (clerk/show! "notebooks/viewers/markdown.clj")
+  (clerk/show! "notebooks/viewers/html.clj")
+
+  (clerk/show! "notebooks/sicmutils.clj")
+
+  (clerk/clear-cache!)
+  )
